9106516e52706374e0507ce5b8dc57a66713e700
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-25 00:26:05 +0100

    adding event listeners

--- game.js
+++ game.js
@@ -9,6 +9,40 @@ const pressingKeys = {
   right: false,
 };
 
+window.addEventListener("keydown", (e) => {
+  switch (e.key.toLowerCase()) {
+    case "arrowup":
+      pressingKeys.up = true;
+      break;
+    case "arrowdown":
+      pressingKeys.down = true;
+      break;
+    case "arrowleft":
+      pressingKeys.left = true;
+      break;
+    case "arrowright":
+      pressingKeys.right = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key.toLowerCase()) {
+    case "arrowup":
+      pressingKeys.up = false;
+      break;
+    case "arrowdown":
+      pressingKeys.down = false;
+      break;
+    case "arrowleft":
+      pressingKeys.left = false;
+      break;
+    case "arrowright":
+      pressingKeys.right = false;
+      break;
+  }
+});
+
 function new2dCanvas(id, width, height) {
   const canvas = document.getElementById(id);
   const ctx = canvas.getContext("2d");
