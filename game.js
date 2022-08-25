35d333d84707d860023cf95ce6245b9b71cf0a4d
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-25 02:09:04 +0100

    added basic movement

--- game.js
+++ game.js
@@ -1,4 +1,41 @@
+function new2dCanvas(id, width, height) {
+  const canvas = document.getElementById(id);
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  return [canvas, ctx];
+}
+
+const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
+
+class Snake {
+  constructor() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.w = 10;
+    this.h = 10;
+    this.speed = 5;
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+
+  update() {
+    if (pressingKeys.up) this.y -= this.speed;
+    if (pressingKeys.down) this.y += this.speed;
+    if (pressingKeys.left) this.x -= this.speed;
+    if (pressingKeys.right) this.x += this.speed;
+    if (this.y <= 0) this.y = 0;
+    if (this.y + this.h >= canvas.height) this.y = canvas.height - this.h;
+    if (this.x <= 0) this.x = 0;
+    if (this.x + this.w >= canvas.width) this.x = canvas.width - this.w;
+  }
+}
+
 const state = {
+  snake: new Snake(),
   over: false,
 };
 
@@ -43,19 +80,9 @@ window.addEventListener("keyup", (e) => {
   }
 });
 
-function new2dCanvas(id, width, height) {
-  const canvas = document.getElementById(id);
-  const ctx = canvas.getContext("2d");
-  canvas.width = width;
-  canvas.height = height;
-  return [canvas, ctx];
-}
-
-const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
-
 function handlePlayerSquare() {
-  ctx.fillStyle = "blue";
-  ctx.fillRect(canvas.width / 2, canvas.height / 2, 10, 10);
+  state.snake.draw();
+  state.snake.update();
 }
 
 (function animate() {
